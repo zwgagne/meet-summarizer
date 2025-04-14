eaa498ae219a47ff1108f66ea861edc04ba2bac8
Zacharie-William Gagné <zwgagne@MacBook-Pro-de-Zac.local>
2025-04-14 18:34:25 -0400

    feat(ui): initialized base vite app and reorganized routes with api prefix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5050', 
+    },
+  },
 })
